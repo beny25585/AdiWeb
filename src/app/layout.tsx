c1ae48dfeed6c762fb44b3325f7bfc7090d1e33c
beny25585 <binyamin25585@gmail.com>
2025-10-05 11:46:14 +0300

    fix run error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html >
+    <html>
+      <head>
+        <link rel="icon" href="/Photos/uzanGroup.jpg" type="image/jpeg" />
+        <title>UZAN GROUP</title>
+        <meta
+          name="description"
+          content="A&S Uzan Group - Engineering, Architecture and Construction"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
