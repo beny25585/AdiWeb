a24120f7bd5af213588eeaa73020e2e99c9a4b97
beny25585 <binyamin25585@gmail.com>
2026-01-11 04:13:56 +0200

    new style for home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,25 +4,48 @@ import CliClientsAndCTA from "@/components/ClientsAndCTA";
 import IsolaionComponent from "@/components/insulationCompponent";
 import "@/styles/scroll-pinning.css";
 import SpecialtiesSection from "../../components/SpecialtiesSection";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <div className="scroll-pinning-container">
-      <div className="scroll-pinning-section">
+    <div className="scroll-container">
+      {/* Background */}
+      <div className="background-image">
+        <Image
+          src="https://res.cloudinary.com/ddncjeozb/image/upload/v1767936515/G35_eadrzq.png"
+          alt="Background"
+          fill
+          priority
+          sizes="100vw"
+        />
+      </div>
+      <section className="section">
+        {/* Foreground content */}
         <Hero />
-      </div>
-      <div className="scroll-pinning-section">
+        <div className="spacer" />
         <FeaturedProjects />
+      </section>
+
+      <div className="background-image">
+        <Image
+          src="https://res.cloudinary.com/ddncjeozb/image/upload/v1767868133/23-LIVING_uxenze.png"
+          alt="Background"
+          fill
+          priority
+          sizes="100vw"
+        />
       </div>
-      <div className="scroll-pinning-section">
+      <section className="section ">
         <IsolaionComponent />
-      </div>
-      <div className="scroll-pinning-section">
+        <div className="spacer" />
+
+        {/* Foreground content */}
         <SpecialtiesSection />
-      </div>
-      <div className="scroll-pinning-section">
+      </section>
+
+      <section className="section">
         <CliClientsAndCTA />
-      </div>
+      </section>
     </div>
   );
 }
